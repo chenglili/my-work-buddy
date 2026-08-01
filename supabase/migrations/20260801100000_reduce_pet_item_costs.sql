51f95ad39118957036c34ee6ef1d78a7a9ab0b09
chenglili <798636989@qq.com>
2026-08-01 18:05:00 +0800

    fix: select mobile Chinese female voices

---
+++ supabase/migrations/20260801100000_reduce_pet_item_costs.sql
@@ -0,0 +1,15 @@
+update public.pet_item_definitions
+set cost = 3
+where id = 'parrot-food';
+
+update public.pet_item_definitions
+set cost = 4
+where id = 'apple-bites';
+
+update public.pet_item_definitions
+set cost = 20
+where id = 'bell-toy';
+
+update public.pet_item_definitions
+set cost = 4
+where id = 'bath-spray';
